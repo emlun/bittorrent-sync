a630aceb1d6dfdf1c4b82ea889fe6477920d8d97
Emil Lundberg <lundberg.emil@gmail.com>
2013-10-07 13:34:39 +0900

    Whitespace
    Replace all tab characters with 4 spaces, and fix indentation.

--- update-checksums.sh
+++ update-checksums.sh
@@ -15,9 +15,9 @@
 # specified, it will not be given one by this script.
 
 if [[ $0 == '/bin/bash' ]]; then
-	echo "It looks like you're sourcing this script."
-	echo "Please execute it directly instead, lest it clutter your shell with variables."
-	return 1
+    echo "It looks like you're sourcing this script."
+    echo "Please execute it directly instead, lest it clutter your shell with variables."
+    return 1
 fi
 
 grep -vE "^[[:space:]]*(if|elif|else|fi)" PKGBUILD > PKGBUILD.geninteg
@@ -25,7 +25,7 @@ grep -vE "^[[:space:]]*(if|elif|else|fi)" PKGBUILD > PKGBUILD.geninteg
 # Compute the new checksums
 newsums=( )
 for l in $(makepkg -g -p PKGBUILD.geninteg | grep -oE "[0-9a-f]{32,}"); do
-	newsums+=("$l")
+    newsums+=("$l")
 done
 
 rm PKGBUILD.geninteg
@@ -33,19 +33,19 @@ rm PKGBUILD.geninteg
 # Find the old checksums
 oldsums=( )
 for l in $(grep -oE "[0-9a-f]{32,}|SKIP" PKGBUILD); do
-	oldsums+=("$l")
+    oldsums+=("$l")
 done
 
 # Update the checksums in PKGBUILD
 echo "Updating checksums..."
 SCRIPT_NAME=$0.sed
 for i in ${!oldsums[@]}; do
-	if [ "${oldsums[i]}" == 'SKIP' ]; then
-		echo 'SKIP > SKIP'
-	else
-		echo "${oldsums[i]} > ${newsums[i]}"
-		echo "s/${oldsums[i]}/${newsums[i]}/" >> $SCRIPT_NAME
-	fi
+    if [ "${oldsums[i]}" == 'SKIP' ]; then
+        echo 'SKIP > SKIP'
+    else
+        echo "${oldsums[i]} > ${newsums[i]}"
+        echo "s/${oldsums[i]}/${newsums[i]}/" >> $SCRIPT_NAME
+    fi
 done
 
 sed -i.bak -f $SCRIPT_NAME PKGBUILD && rm $SCRIPT_NAME
